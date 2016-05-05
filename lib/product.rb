dbc4dbba7f2c769dbc1f114000be732f102b019d
Mike Johns <hello@mikejohns.email>
2016-05-05 15:44:33 -0700

    Added rudimentary DuplicateProductError and method to check for a duplicate when a product is created.

--- lib/product.rb
+++ lib/product.rb
@@ -1,12 +1,22 @@
 class Product
+  attr_reader :title
+
   @@products = []
 
   def initialize(options = {})
-    @@products << self
+    @title = options[:title]
+    add_to_products
   end
 
   def self.all
     @@products
+  end
+
+  private
+
+  def add_to_products
+    @@products.each {|product| product.title == @title ? raise(DuplicateProductError, "'#{@title}' already exists.") : nil}
+    @@products << self
   end
 
 end
