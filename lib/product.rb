c6e3943b8ba559d60df9c1c35e4743adf21b2b50
Mike Johns <hello@mikejohns.email>
2016-05-28 21:23:01 -0700

    Finished Product class methods.

--- lib/product.rb
+++ lib/product.rb
@@ -1,15 +1,36 @@
 class Product
-  attr_reader :title
+  attr_reader :title, :price, :stock
 
   @@products = []
 
   def initialize(options = {})
     @title = options[:title]
+    @price = options[:price]
+    @stock = options[:stock]
     add_to_products
   end
 
   def self.all
     @@products
+  end
+
+  def self.find_by_title(search_title)
+    @@products.each {|product| product.title == search_title ? (return product) : nil}
+    puts "Nothing found"
+  end
+
+  def self.in_stock
+    products_in_stock = []
+    @@products.each {|product| (product.stock != 0 && product.stock != nil) ? products_in_stock << product : nil}
+    products_in_stock
+  end
+
+  def in_stock?
+    if @stock == 0 || @stock == nil
+      false
+    else
+      true
+    end
   end
 
   private
